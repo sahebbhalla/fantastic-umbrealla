5d0800f8411334544bca060e6c3da43eafd9efdc
saheb <sahibbhalla.010@gmail.com>
2022-10-08 11:32:38 -0400

    Completed product routes with delete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -140,7 +140,22 @@ router.put("/:id", (req, res) => {
 });
 
 router.delete("/:id", (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No Product found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
